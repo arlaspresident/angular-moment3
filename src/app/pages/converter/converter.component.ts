88ccf66320286ac26b78f1b7867eb8e87d9833d0
arlaspresident <jankovic4901@gmail.com>
2025-04-27 10:09:17 +0200

    converter klar

--- src/app/pages/converter/converter.component.ts
+++ src/app/pages/converter/converter.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './converter.component.html',
   styleUrls: ['./converter.component.scss'],
   standalone: true 
 })
 export class ConverterComponent {
+  meters = 0;
+  feet = 0;
 
+  celsius = 0;
+  fahrenheit = 0;
+
+  convertToFeet() {
+    this.feet = parseFloat((this.meters * 3.28084).toFixed(2));
+  }
+
+  convertToMeters() {
+    this.meters = parseFloat((this.feet / 3.28084).toFixed(2));
+  }
+
+  convertToFahrenheit() {
+    this.fahrenheit = parseFloat(((this.celsius * 9 / 5) + 32).toFixed(2));
+  }
+
+  convertToCelsius() {
+    this.celsius = parseFloat(((this.fahrenheit - 32) * 5 / 9).toFixed(2));
+  }
 }
